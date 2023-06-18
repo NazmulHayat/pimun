784b5804346e8c283f1742f617f6fa5af7e2f63e
NazmulHayat <itsmehnaz05@gmail.com>
2023-06-19 02:27:33 +0600

    third update

--- src/components/thirdpage.vue
+++ src/components/thirdpage.vue
@@ -32,7 +32,21 @@
   <v-row>
     <template v-for="(image, imgidx) in imageLayout" :key="imgidx">
       <v-col id="im" :cols="image.cols">
-        <v-img :src="require(`@/assets/compressed/${imgidx+1}-compressed.jpg`)" cover height = "100%"></v-img>
+        <v-img :src="require(`@/assets/compressed/${imgidx+1}-compressed.jpg`)" cover height = "100%">
+          <template v-slot:placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular
+              indeterminate
+              color="grey-lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+        
+        </v-img>
       </v-col>
       
     </template>
