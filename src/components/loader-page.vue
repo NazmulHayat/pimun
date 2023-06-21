dba756622d51e193bbb34b9f2ff514e7d38ed6d2
NazmulHayat <itsmehnaz05@gmail.com>
2023-06-21 23:26:12 +0600

    bhalllengaaa finallly

--- src/components/loader-page.vue
+++ src/components/loader-page.vue
@@ -1,31 +1,63 @@
 <template>
-    <div class="page-loader" v-if="!isloaded">
-      <div class="cube"></div>
-      <div class="cube"></div>
-      <div class="cube"></div>
-      <div class="cube"></div>
-    </div> 
-  </template>
+  <div class="page-loader" v-if="!isloaded">
+    <div class="cube"></div>
+    <div class="cube"></div>
+    <div class="cube"></div>
+    <div class="cube"></div>
+  </div>
+</template>
   
-  <script>
-    export default {
-      data: () => {
-        return {
-          isloaded: false
-        }
-      },
-      mounted() {
-        document.onreadystatechange = () => {
-          if (document.readyState == "complete") { 
-            this.isloaded = true;
-          } 
-        }
-      },
+<script>
+
+function preloadImages(urls, allImagesLoadedCallback) {
+  var loadedCounter = 0;
+  var toBeLoadedNumber = urls.length;
+  urls.forEach(function (url) {
+    preloadImage(url, function () {
+      loadedCounter++;
+      console.log('Number of loaded images: ' + loadedCounter);
+      if (loadedCounter == toBeLoadedNumber) {
+        allImagesLoadedCallback();
+      }
+    });
+  });
+  function preloadImage(url, anImageLoadedCallback) {
+    var img = new Image();
+    img.onload = anImageLoadedCallback;
+    img.src = url;
+  }
+}
+
+// Let's call it: NO
+
+
+export default {
+  data: () => {
+    return {
+      isloaded: false
     }
-  </script>
+  },
+  mounted() {
+    document.onreadystatechange = () => {
+      if (document.readyState == "complete") {
+        preloadImages([
+          "../assets/golgal.png",
+          '../assets/og1.jpg',
+          "../assets/pimuncollage.png",
+          "../assets/bg-page.jpg"
+        ], function () {
+          console.log('All images were loaded');
+          this.isloaded = true;
+        });
+        
+      }
+    }
+  },
+}
+</script>
   
-  <style lang="scss" scoped>
- .page-loader {
+<style lang="scss" scoped>
+.page-loader {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -37,44 +69,53 @@
   background-color: #333;
   z-index: 999;
 }
+
 .cube {
   width: 40px;
   height: 40px;
   margin-right: 10px;
 }
+
 .cube:nth-child(1) {
   background-color: #8CC271;
 }
+
 .cube:nth-child(2) {
   background-color: #69BEEB;
 }
+
 .cube:nth-child(3) {
   background-color: #F5AA39;
 }
+
 .cube:nth-child(4) {
   background-color: #E9643B;
 }
+
 .cube:first-child {
   animation: left 1s infinite;
 }
+
 .cube:last-child {
   animation: right 1s infinite 0.5s;
 }
+
 @keyframes left {
   40% {
     transform: translateX(-60px);
   }
-  50% {
-    transform: translateX(0);
-  }
-}
-@keyframes right {
-  40% {
-    transform: translateX(60px);
-  }
+
   50% {
     transform: translateX(0);
   }
 }
 
-  </style>
+@keyframes right {
+  40% {
+    transform: translateX(60px);
+  }
+
+  50% {
+    transform: translateX(0);
+  }
+}</style>
